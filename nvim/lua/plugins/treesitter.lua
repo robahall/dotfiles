a97668e7c56240eeb3127a45ab38164c8035b8ab
Rob Hall <robahall2@gmail.com>
2025-08-11 07:52:50 -0400

    Updated .dotfiles

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,62 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "python",
+          "javascript",
+          "typescript",
+          "json",
+          "yaml",
+          "html",
+          "css",
+          "rust",
+          "go",
+          "c",
+          "cpp",
+        },
+        auto_install = true,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
